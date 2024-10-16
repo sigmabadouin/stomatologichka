058687b7b78640c1705005a4606d5efa3ca74f70
sigmabadouin <azreqr@gmail.com>
2024-10-16 19:23:09 +0400

    test1

--- backend/backend.py
+++ backend/backend.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def mainpage():
     return render_template('mainpage.html')
 
+@app.route('/usluga1')
+def usluga1():
+    return render_template('usluga1.html')
+
+@app.route('/usluga2')
+def usluga2():
+    return render_template('usluga2.html')
+
+
+@app.route('/usluga3')
+def usluga3():
+    return render_template('usluga3.html')
+
 
 
 if __name__ == "__main__":
