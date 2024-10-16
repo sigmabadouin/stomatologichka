25119c0ac83941519ebead00a605570ecd061bfe
sigmabadouin <azreqr@gmail.com>
2024-10-16 20:00:09 +0400

    123

--- backend/backend.py
+++ backend/backend.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/mainpage")
 def mainpage():
     return render_template('index.html')
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
         app.run(debug=True)
