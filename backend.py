bf6bc5d42070a50d856229b40e175081e8435358
sigmabadouin <azreqr@gmail.com>
2024-10-16 22:43:58 +0400

    небольшй допил

--- backend.py
+++ backend.py
@@ -8,15 +8,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/usluga1")
+@app.route("/terapevticheskaya_stomatologiya")
 def usluga1():
     return render_template('usluga1.html')
 
-@app.route("/usluga2")
+@app.route("/usluga_2")
 def usluga2():
     return render_template('usluga2.html')
 
-@app.route("/usluga3")
+@app.route("/usluga_3")
 def usluga3():
     return render_template('usluga3.html')
 
