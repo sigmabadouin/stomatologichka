63e3a797aaf13c967debe3641b2211daad18045e
sigmabadouin <azreqr@gmail.com>
2024-10-24 00:14:54 +0400

    доработки

--- templates/buttons.js
+++
@@ -1,8 +0,0 @@
-/* какой-то пиздец который я не понимаю*/
-
-document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const panel = button.nextElementSibling;
-        panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
-    });
-});
